26dca01a092d504012c06f8312ff60aa7d456970
Timilehin Vincent <timilehinvincent@gmail.com>
2021-09-25 00:20:00 +0100

    Adding @Autowired to Service layer and making the view layer display text passed to it.

--- src/main/java/com/timilehinvincent/springbootjokeapp/controllers/JokeController.java
+++ src/main/java/com/timilehinvincent/springbootjokeapp/controllers/JokeController.java
@@ -1,6 +1,7 @@
 package com.timilehinvincent.springbootjokeapp.controllers;
 
 import com.timilehinvincent.springbootjokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ public class JokeController {
 
     JokeService jokeService;
 
+    @Autowired
     public void setJokeService(JokeService jokeService) {
         this.jokeService = jokeService;
     }
